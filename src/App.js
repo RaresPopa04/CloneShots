05c66c79135153336b7b5008067040acff1749a4
Popa Rares Andrei <R.A.Popa@student.tudelft.nl>
2024-07-15 08:25:29 +0200

    Minimal phone done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { useEffect, useRef, useState } from 'react';
 import html2canvas from 'html2canvas';
 import './App.css';
 import ScreenshotPosition from './ScreenshotPositions';
+import MinimalPhonePosition from './MinimalPhonePosition';
 
 function App() {
 	const [preview, setPreview] = useState(null);
 	const [previewTwo, setPreviewTwo] = useState(null);
-	const [twoCanvases, setTwoCanvases] = useState(false);
-	const [bo, setSliderValue] = useState(50);
+	const [previewThree, setPreviewThree] = useState(null);
 	
+	const [layoutsName, setLayoutsName] = useState("layout-0");
 	const ref = useRef();
 
 	const layoutsNames = ["Screenshot", "Browser","Minimal Phone","Minimal Desktop","Iphone15","Iphone 15 Plus", "Iphone 15 Pro", "Iphone 15 Pro Max","Nothing Phone","Pixel 7 Pro","Iphone 14","Iphone 14 Plus","Iphone 14 Pro", "Iphone 14 Pro Max","Iphone 13","Iphone 13 Pro","Iphone SE","iPad Pro 11","iPad Pro 13","iPad Air","iPadMini","Macbook Pro 16","Macbook Air M2","Mackbook Air 13","Macbook Pro 13","Macbook Pro 15","Surface Book","iMac24","Pro Display XDR","Studio Display","iMac Pro","iMax Retina","Apple Watch Ultra","Apple Watch Series 8"]
@@ -283,103 +284,107 @@ function App() {
 			});
 		}
 	}
-	const handleFileChange = (e) => {
+	const handleFileChange = (e,num) => {
 		const file = e.target.files[0];
 		if(file &&(file.type === 'image/jpeg' || file.type === 'image/png') ){
 			const reader = new FileReader();
 			reader.onloadend = () => {
-				setPreview(reader.result);
-				
+				if(num == 1)
+					setPreview(reader.result);
+				else if(num == 2)
+					setPreviewTwo(reader.result);
+				else if(num == 3)
+					setPreviewThree(reader.result); 
+				console.log(num + " "+ previewTwo);
 				const img = new Image();
 				img.onload = ()=>{
 					const width = img.width;
 					const height = img.height;
 					const aspectRatio = width/height;
-					document.documentElement.style.setProperty('--aspect-ratio', aspectRatio);
-					console.log(aspectRatio);
+					if(layoutsName=="layout-0")
+						if(num == 1)
+							document.documentElement.style.setProperty('--aspect-ratio', aspectRatio);
+						else if (num == 2)
+							document.documentElement.style.setProperty('--aspect-ratio2', aspectRatio);
+						else if (num == 3)
+							document.documentElement.style.setProperty('--aspect-ratio3', aspectRatio);
 				}
 				img.src = reader.result;
 			}
 			reader.readAsDataURL(file);
 		}
 		else{
-			setPreview(null);
-			alert("Please select a valid file type");
-		}
-	}
-	const applyStyleTwo = (styleNumber) => {
-		const dropboxParents = document.querySelectorAll('.canvas .uploadBox');
-		const centeredDropbox = document.querySelector('.canvas .centeredDropbox');
-		centeredDropbox.classList.add('two-canvases');
-		dropboxParents[1].classList.remove('none');
-		
-		for(let attri of centeredDropbox.classList){
-			if(attri.includes("style-") || attri.includes("styletwo-")){
-				centeredDropbox.classList.remove(attri);
-			}
-		}
-		centeredDropbox.classList.add(`styletwo-${styleNumber}`)
-		const button = document.querySelector(`.card-wrapper .card:nth-child(${4+styleNumber+1})`);
-		const activeButtons = document.querySelectorAll('.card-wrapper .card.active');
-		activeButtons.forEach((button)=>{
-			button.classList.remove('active');
-		})
-		button.classList.add('active');
-		
-		
-	}
-	const handleClickInputFile = () => {
-		
-		const inputFile = document.getElementById("fileInput");
-		inputFile.click();
-	}
-	
-	const handleClickInputFileTwo = () => {
-		
-		const inputFile = document.getElementById("fileInputTwo");
-		inputFile.click();
-	}
-	
-	const handleFileChangeTwo = (e) => {
-		const file = e.target.files[0];
-		if(file &&(file.type === 'image/jpeg' || file.type === 'image/png') ){
-			const reader = new FileReader();
-			reader.onloadend = () => {
-				setPreviewTwo(reader.result);
-				
-				const img = new Image();
-				img.onload = ()=>{
-					const width = img.width;
-					const height = img.height;
-					const aspectRatio = width/height;
-					document.documentElement.style.setProperty('--aspect-ratio2', aspectRatio);
-					console.log(aspectRatio);
-				}
-				img.src = reader.result;
-			}
-			reader.readAsDataURL(file);
-		}
-		else{
-			setPreviewTwo(null);
+			if(num == 1)
+				setPreview(null);
+			else if(num == 2)
+				setPreviewTwo(null);
+			else if(num == 3)
+				setPreviewThree(null);
 			alert("Please select a valid file type");
 		}
 	}
 	
 	
+	const handleClickInputFile = (num) => {
+		let inputFile;
+		console.log(num);
+		if(num == 1)
+			inputFile = document.getElementById("fileInput");
+		else if(num == 2)
+			inputFile = document.getElementById("fileInputTwo");
+		else if(num == 3)
+			inputFile = document.getElementById("fileInputThree");
+		inputFile.click();
+	}
 	
-	const applyStyle = (styleNumber,two) => {
+	
+	
+	
+	const applyStyle = (styleNumber,classToAdd,styleNo) => {
 		const hideDropbox = document.querySelectorAll('.canvas .uploadBox');
-		hideDropbox[1].classList.add('none');
+		if(styleNo == 0){
+			hideDropbox[1].classList.add("none");
+			hideDropbox[2].classList.add("none");
+		}else if (styleNo == 1){
+			hideDropbox[1].classList.remove("none");
+			hideDropbox[2].classList.add("none");
+		}else if (styleNo == 2){
+			hideDropbox[1].classList.remove("none");
+			hideDropbox[2].classList.remove("none");
+		}
+
 		const centeredDropbox = document.querySelector('.canvas .centeredDropbox');
-		centeredDropbox.classList.remove('two-canvases');
+		if(styleNo == 0)
+		{
+			centeredDropbox.classList.remove('two-canvases');
+			centeredDropbox.classList.remove('three-canvases');
+		}else if(styleNo == 1)
+		{
+			centeredDropbox.classList.add('two-canvases');
+			centeredDropbox.classList.remove('three-canvases');
+		}else if(styleNo == 2){
+			centeredDropbox.classList.remove('two-canvases');
+			centeredDropbox.classList.add('three-canvases');
+		}
+		
 		
 		const dropboxParent = document.querySelector('.canvas .centeredDropbox');
 		for(let attri of dropboxParent.classList){
-			if(attri.includes('style-') || attri.includes('styletwo-')){
+			if(attri.includes('style-') || attri.includes('styletwo-') || attri.includes("stylethree-")){
+
 				dropboxParent.classList.remove(attri);
 			}
 		}
-		dropboxParent.classList.add(`style-${styleNumber}`)
+		console.log("applying")
+		console.log(dropboxParent.classList)
+		if(styleNo == 0)
+			dropboxParent.classList.add(`style-${styleNumber}`)
+		else if(styleNo == 1)
+			dropboxParent.classList.add(`styletwo-${styleNumber}`)
+		else if(styleNo == 2)
+			dropboxParent.classList.add(`stylethree-${styleNumber}`)
+
+		dropboxParent.classList.add(classToAdd);
 		
 		const button = document.querySelector(`.card-wrapper .card:nth-child(${styleNumber+1})`);
 		const activeButtons = document.querySelectorAll('.card-wrapper .card.active');
@@ -857,8 +862,14 @@ function App() {
 		layoutPanel.classList.toggle('none');
 	}
 
-	const [layoutsName, setLayoutsName] = useState("Screenshot");
+	const changeLayout = (aspectRatio,textRatio)=>{
+		document.documentElement.style.setProperty('--aspect-ratio',aspectRatio);
+		document.documentElement.style.setProperty('--aspect-ratio2', aspectRatio);
+		document.documentElement.style.setProperty('--aspect-ratio3', aspectRatio);
 
+		document.querySelector(".infoElem p").innerHTML = textRatio;
+		document.querySelector(".selector .details span").innerHTML = textRatio;
+	}
 	const handleLayoutChange = (e,index) => {
 		const pressedButton = e.currentTarget;
 		const activeButton = document.querySelector(".layoutPanel .list .grid button.active");
@@ -870,15 +881,22 @@ function App() {
 		const title = document.querySelector(".selector .details p")
 		title.innerHTML = layoutsNames[index];
 		coverImg.src = cover[index];
-
-		if(index === 2){
-			document.documentElement.style.setProperty('--aspect-ratio', 6/13);
-			document.documentElement.style.setProperty('--aspect-ratio2', 6/13);
-			document.documentElement.style.setProperty('--aspect-ratio3', 6/13);
-
-			document.querySelector(".infoElem p").innerHTML = "6:13";
+		console.log(index);
+		setLayoutsName(`layout-${index}`)
+		switch(index){
+			case 0:
+				changeLayout(1/1,"Adapts to image")
+				break;
+			case 2:
+				changeLayout(6/13, "6:13")
+				break;
 		}
-
+		const dropboxParent = document.querySelector('.canvas .centeredDropbox');
+		for(let attri of dropboxParent.classList){
+			if(attri.includes('style-') || attri.includes('styletwo-') || attri.includes("stylethree-") || attri.includes("layout-")){
+				dropboxParent.classList.remove(attri);
+			}
+		}
 	}
 	
 	return (
@@ -1332,8 +1350,9 @@ function App() {
 				</div>
 				
 			</div>
-			<input type="file" onChange = {handleFileChange} id="fileInput" style={{display:"none"}}/>
-			<input type="file" onChange = {handleFileChangeTwo} id="fileInputTwo" style={{display:"none"}}/>
+			<input type="file" onChange = {(e)=>handleFileChange(e,1)} id="fileInput" style={{display:"none"}}/>
+			<input type="file" onChange = {(e)=>handleFileChange(e,2)} id="fileInputTwo" style={{display:"none"}}/>
+			<input type="file" onChange = {(e)=>handleFileChange(e,3)} id="fileInputThree" style={{display:"none"}}/>
 			
 			<div className="canvas" >
 				<div className="canvasContainer">
@@ -1343,10 +1362,10 @@ function App() {
 							</div>
 							<div className="dropboxParent">
 								<div className="dropbox">
-									<div className="centeredDropbox style-0" >
+									<div className={`centeredDropbox style-0 ${layoutsName}`} >
 										
 									
-										<div className="uploadBox" onClick={handleClickInputFile} >
+										<div className="uploadBox" onClick={()=>handleClickInputFile(1)} >
 											
 											<div className='stack2 none'>
 												{
@@ -1379,7 +1398,7 @@ function App() {
 													<img src={preview} alt="preview" />
 												</div>}
 										</div>
-										<div className="uploadBox none" onClick={handleClickInputFileTwo}>
+										<div className="uploadBox none" onClick={()=>handleClickInputFile(2)}>
 											<div className='stack2 none'>
 													{
 														previewTwo &&
@@ -1411,6 +1430,39 @@ function App() {
 													<img src={previewTwo} alt="preview" />
 												</div>}
 										</div>
+										<div className="uploadBox none" onClick={()=>handleClickInputFile(3)}>
+											<div className='stack2 none'>
+													{
+														previewThree &&
+														<div className="imgContainer">
+															<img src={previewThree} alt="preview" style={{filter:"blur(2em)"}}/>
+														</div>
+													}
+											</div>
+											<div className='stack1 none'>
+												{previewThree &&
+														<div className="imgContainer">
+															<img src={previewThree} alt="preview" style={{filter:"blur(2em)"}}/>
+														</div>}
+											</div>
+											{previewThree ==null &&
+												<div class="content default">
+													<div class="svg-loader undefined">
+														<div>
+															<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M16.33 2c3.38 0 5.66 2.37 5.66 5.91v8.16c0 3.53-2.28 5.91-5.67 5.91H7.65c-3.39 0-5.67-2.38-5.67-5.92V7.89c0-3.54 2.27-5.92 5.66-5.92h8.66Zm1.1 10.55c-1.08-.67-1.9.27-2.13.57 -.22.29-.41.6-.6.92 -.48.78-1.03 1.69-1.97 2.22 -1.38.76-2.42.05-3.17-.45 -.29-.19-.56-.37-.83-.49 -.68-.29-1.28.04-2.18 1.17 -.48.59-.94 1.18-1.41 1.77 -.29.35-.22.89.16 1.13 .6.37 1.35.57 2.18.57h8.42c.47 0 .95-.07 1.4-.22 1.02-.34 1.83-1.11 2.26-2.12 .35-.86.532-1.89.19-2.75 -.12-.29-.28-.55-.52-.79 -.62-.61-1.19-1.18-1.88-1.61ZM8.49 6c-1.38 0-2.5 1.12-2.5 2.49s1.12 2.5 2.49 2.5 2.49-1.13 2.49-2.51 -1.13-2.5-2.5-2.5Z"></path></svg>
+														</div>
+													</div>
+													<h3>Drop or Paste</h3>
+													<span>(PNG or JPEG)</span>
+													<p class="screen-info">Adapts to image</p>
+												</div>
+											}
+											{previewThree &&
+												<div className="imgContainer">
+													<img src={previewThree} alt="preview" />
+												</div>}
+										</div>
+										
 										
 									</div>
 									
@@ -1427,7 +1479,15 @@ function App() {
 			
 			<div className="right-bar">
 				<div className="scroller">
-					<ScreenshotPosition preview = {preview} preview2 = {previewTwo} applyStyle={applyStyle} applyStyleTwo = {applyStyleTwo}/>
+					{
+						layoutsName === "layout-0" && 
+						<ScreenshotPosition preview = {preview} preview2 = {previewTwo} applyStyle={applyStyle}/>
+					}
+					{
+						layoutsName === "layout-2" &&
+						<MinimalPhonePosition preview = {preview} preview2 = {previewTwo} preview3 = {previewThree} applyStyle = {applyStyle}/>
+					}
+					
 				</div>
 				
 			</div>
--- src/MinimalPhonePosition.jsx
+++ src/MinimalPhonePosition.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-const ScreenshotPosition = (props)=>{
-    const {preview,preview2:previewTwo} = props;
+const MinimalPhonePosition = (props)=>{
+    const {preview,preview2:previewTwo,preview3:previewThree} = props;
     const applyStyle = props.applyStyle;
     return (
         <div className="card-wrapper">
-          {
-							Array.from({length:4}).map((_,i)=>{
-								return (<div className="card" onClick={()=>applyStyle(i,"layout-0",0)}>
+          				{
+							Array.from({length:2}).map((_,i)=>{
+								return (<div className="card" onClick={()=>applyStyle(i, "layout-2",0)}>
 									<div className="frame" >
 										<div className="innerFrame" >
 											<div className="background">
 											</div>
 											<div className="dropboxParent">
 												<div className="dropbox">
-													<div className={`centeredDropbox style-${i}`} >
+													<div className={`centeredDropbox layout-2 style-${i}`} >
 														<div className="uploadBox">
 															<div className='stack2 none'>
 																{
@@ -56,15 +56,15 @@ const ScreenshotPosition = (props)=>{
 							
 						}
 						{
-							Array.from({length:10}).map((_,i)=>{
-								return (<div className="card" onClick={()=>applyStyle(i,"layout-0",1)}>
+							Array.from({length:5}).map((_,i)=>{
+								return (<div className="card" onClick={()=>applyStyle(i,"layout-2",1)}>
 									<div className="frame" >
 										<div className="innerFrame">
 											<div className="background">
 											</div>
 											<div className="dropboxParent">
 												<div className="dropbox">
-													<div className={`centeredDropbox two-canvases styletwo-${i}`} >
+													<div className={`centeredDropbox layout-2 two-canvases styletwo-${i}`} >
 														<div className="uploadBox" >
 															<div className='stack2 none'>
 																{
@@ -128,16 +128,118 @@ const ScreenshotPosition = (props)=>{
 								</div>)
 							})
 						}
-            {
-							Array.from({length:2}).map((_,i)=>{
-								return (<div className="card" onClick={()=>applyStyle(i+14,"layout-0",0)}>
+           				{
+							Array.from({length:5}).map((_,i)=>{
+								return (<div className="card" onClick={()=>applyStyle(i,"layout-2",2)}>
 									<div className="frame" >
 										<div className="innerFrame">
 											<div className="background">
 											</div>
 											<div className="dropboxParent">
 												<div className="dropbox">
-													<div className={`centeredDropbox style-${i+14}`} >
+													<div className={`centeredDropbox three-canvases layout-2 stylethree-${i}`} >
+														<div className="uploadBox">
+															<div className='stack2 none'>
+																{
+																	preview &&
+																	<div className="imgContainer">
+																		<img src={preview} alt="preview" style={{filter:"blur(2em)"}}/>
+																	</div>
+																}
+															</div>
+															<div className='stack1 none'>
+																{preview &&
+																		<div className="imgContainer">
+																			<img src={preview} alt="preview" style={{filter:"blur(2em)"}}/>
+																		</div>}
+															</div>
+															{preview ==null &&
+																<div class="content default">
+																	
+																</div>
+															}
+															{preview &&
+																<div className="imgContainer">
+																	<img src={preview} alt="preview" />
+																</div>}
+														</div>
+														<div className="uploadBox">
+															<div className='stack2 none'>
+																{
+																	previewTwo &&
+																	<div className="imgContainer">
+																		<img src={previewTwo} alt="preview" style={{filter:"blur(2em)"}}/>
+																	</div>
+																}
+															</div>
+															<div className='stack1 none'>
+																{previewTwo &&
+																		<div className="imgContainer">
+																			<img src={previewTwo} alt="preview" style={{filter:"blur(2em)"}}/>
+																		</div>}
+															</div>
+															{previewTwo ==null &&
+																<div class="content default">
+																	
+																</div>
+															}
+															{previewTwo &&
+																<div className="imgContainer">
+																	<img src={previewTwo} alt="preview" />
+																</div>}
+														</div>
+														
+														<div className="uploadBox">
+															<div className='stack2 none'>
+																{
+																	previewThree &&
+																	<div className="imgContainer">
+																		<img src={previewThree} alt="preview" style={{filter:"blur(2em)"}}/>
+																	</div>
+																}
+															</div>
+															<div className='stack1 none'>
+																{previewThree &&
+																		<div className="imgContainer">
+																			<img src={previewThree} alt="preview" style={{filter:"blur(2em)"}}/>
+																		</div>}
+															</div>
+															{previewThree ==null &&
+																<div class="content default">
+																	
+																</div>
+															}
+															{previewThree &&
+																<div className="imgContainer">
+																	<img src={previewThree} alt="preview" />
+																</div>}
+														</div>
+														
+														
+														
+													</div>
+													
+													
+												</div>
+												
+											</div>
+										</div>
+										
+									</div>
+								</div>)
+							})
+							
+						}
+						{
+							Array.from({length:2}).map((_,i)=>{
+								return (<div className="card" onClick={()=>applyStyle(i+2,"layout-2",0)}>
+									<div className="frame" >
+										<div className="innerFrame">
+											<div className="background">
+											</div>
+											<div className="dropboxParent">
+												<div className="dropbox">
+													<div className={`centeredDropbox layout-2 style-${i+2}`} >
 														<div className="uploadBox">
 															<div className='stack2 none'>
 																{
@@ -180,9 +282,10 @@ const ScreenshotPosition = (props)=>{
 						}
 						
 						
+						
 		</div>
     )
 
 }
 
-export default ScreenshotPosition;
+export default MinimalPhonePosition;
